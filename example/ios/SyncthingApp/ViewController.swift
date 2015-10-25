f709ac87c1708428074839fcc2ef7e451caa32e1
Leonard Chioveanu <leonard.chioveanu@sparktech.ro>
2015-10-25 16:05:19 +0200

    iOS: Properly reload if url failed.

--- example/ios/SyncthingApp/ViewController.swift
+++ example/ios/SyncthingApp/ViewController.swift
@@ -13,6 +13,7 @@ public class ViewController: UIViewController {
     
     @IBOutlet weak var webView: UIWebView?
     @IBOutlet weak var backButton: UIBarButtonItem?
+    var loadedOnce: Bool?
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -25,30 +26,48 @@ public class ViewController: UIViewController {
     override public func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         
-        webView?.loadRequest(NSURLRequest(URL: NSURL(string: "http://127.0.0.1:8384")!))
+        reload()
     }
     
     @IBAction func reload(sender: AnyObject) {
-        webView?.reload()
+        reload()
     }
     
     @IBAction func back(sender: AnyObject) {
         webView?.goBack()
+    }
+    
+    func reload() {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+        
+        if let _ = loadedOnce {
+            webView?.reload()
+        }
+        else {
+            webView?.loadRequest(NSURLRequest(URL: NSURL(string: "http://127.0.0.1:8384")!))
+        }
     }
 }
 
 extension ViewController: UIWebViewDelegate {
     
     public func webViewDidFinishLoad(webView: UIWebView) {
+        self.loadedOnce = true
         self.title = webView.request?.URL?.absoluteString
         self.backButton?.enabled = webView.canGoBack
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
     
     public func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
-        let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
-        dispatch_after(delayTime, dispatch_get_main_queue()) {
-            webView.reload()
+        guard let _ = loadedOnce else {
+            let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(1 * Double(NSEC_PER_SEC)))
+            dispatch_after(delayTime, dispatch_get_main_queue()) {
+                self.reload()
+            }
+            
+            return;
         }
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
     }
 }
 
